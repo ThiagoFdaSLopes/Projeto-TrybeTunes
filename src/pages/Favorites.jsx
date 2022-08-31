fecad2d70034d42f8460b1308605a5b5dcb2db8b
Thiago <programadorthiagolopes@gmail.com>
2022-08-31 16:55:26 -0300

    Feito requisito 12

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,67 @@
+import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
+import MusicCard from '../components/MusicCard';
+import Carregando from './Carregando';
 
-function Favorites() {
-  return (
-    <div data-testid="page-favorites">
-      <Header />
-    </div>
-  );
+class Favorites extends Component {
+  state = {
+    musicas: [],
+    carregando: false,
+  };
+
+  async componentDidMount() {
+    await this.getMusicasFavoritas();
+  }
+
+  removendoFavoritas = async (e) => {
+    // Preciso renderizar novaLista
+    this.setState({ carregando: true }, async () => {
+      await removeSong(e);
+      await this.getMusicasFavoritas();
+      this.setState({ carregando: false });
+    });
+  };
+
+  getMusicasFavoritas = async () => {
+    const musics = await getFavoriteSongs();
+    this.setState({
+      musicas: musics,
+    });
+  };
+
+  render() {
+    const { musicas, carregando } = this.state;
+    return (
+      <div>
+        <Header />
+        {carregando ? <Carregando /> : musicas.map((e, index) => (
+          <div key={ e.trackId }>
+            <MusicCard
+              key={ e.trackNumber }
+              trackName={ e.trackName }
+              previewUrl={ e.previewUrl }
+            />
+            <label
+              htmlFor={ index }
+            >
+              Favorita
+              <input
+                type="checkbox"
+                id={ index }
+                onClick={ async () => {
+                  await this.removendoFavoritas(e);
+                } }
+                data-testid={ `checkbox-music-${e.trackId}` }
+                defaultChecked={ musicas.length > 0 && (
+                  musicas.some((e2) => e2.trackName === e.trackName)
+                ) }
+              />
+            </label>
+          </div>))}
+      </div>
+    );
+  }
 }
 
 export default Favorites;
